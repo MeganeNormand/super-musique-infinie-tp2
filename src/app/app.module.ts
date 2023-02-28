b5d170af0e1a18ed70534184b11c8efdf812f12e
Megane <2150130@cegepmontpetit.ca>
2023-02-27 19:36:55 -0500

    Component artiste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArtisteComponent } from './artiste/artiste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtisteComponent
   ],
   imports: [
     BrowserModule
