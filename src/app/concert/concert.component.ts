5d420d2d1f5445c008ad0eaafca68cf54fe3697f
Megane <2150130@cegepmontpetit.ca>
2023-02-28 22:25:13 -0500

    Maps init

--- src/app/concert/concert.component.ts
+++ src/app/concert/concert.component.ts
@@ -12,19 +12,33 @@ import { Loader } from "@googlemaps/js-api-loader"
 export class ConcertComponent implements OnInit {
   artisteName: string | null = null;
 
-  constructor(private bandintown: BandsintownService, public route: ActivatedRoute) { 
+  constructor(private bandintown: BandsintownService, public route: ActivatedRoute) {
     this.artisteName = this.route.snapshot.paramMap.get("artisteName");
   }
 
-  
+
   concertList: Concert[] = [];
-    
+  myLatLng = { lat: 46.829853, lng: -71.254028 };
+
 
   ngOnInit(): void {
-    
+    this.initMAp();
+    this.afficherConcert();
+
+  }
+
+  async afficherConcert() {
+    if (this.artisteName != null) {
+      this.concertList = await this.bandintown.getConcert(this.artisteName);
+    }
+    console.log(this.concertList);
+  }
+
+  initMAp() {
     //MAP
     let map: google.maps.Map;
-    
+
+
     const loader = new Loader({
       apiKey: "AIzaSyA5BQYMsqPdmvNN6PQWix5HsRX15wsYSsA",
       version: "weekly"
@@ -33,22 +47,18 @@ export class ConcertComponent implements OnInit {
     loader.load().then(() => {
       map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
         center: { lat: 46.829853, lng: -71.254028 },
-        zoom: 5
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
       });
+
+      var marker = new google.maps.Marker({
+        position: this.myLatLng,
+        title: "Hello World!"
+      });
+
+      marker.setMap(map);
     });
-      
-    this.afficherConcert();
-
   }
+
   
-  async afficherConcert(){
-    if(this.artisteName != null){
-      this.concertList = await this.bandintown.getConcert(this.artisteName);
-    }
-    console.log(this.concertList);
-  }
-
- 
-
-
 }
